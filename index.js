a3d99c26d050a489d022008b43d2c4b25388d560
Jordan Tehranchi <jordan.tehranchi@yahoo.com>
2022-04-06 19:49:38 -0700

    Added BTC,ETH,XRP nomics api using ExpressJS and Node.JS node-fetch

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-var express = require("express");
+
+
+import express from 'express';
+import fetch from 'node-fetch';
 var app = express();
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-   });
 
-app.listen(4000, function() {
-  console.log("Server is running on port 4000");
+
+// PORT NUMBER
+const port = process.env.PORT || 4000;
+app.listen(port, function() {
+  console.log(`Server is running on port ${port}`);
 });
+
+
+
+app.get('/', (req, res) => {
+    const api_key = '566cd509fae9b7d8663706cef40a59d3f26dbc8e';
+    const url_api = `https://api.nomics.com/v1/currencies/ticker?key=${api_key}&ids=BTC,ETH,XRP&interval=1d`;
+    fetch(url_api)
+        .then(res => res.json())
+        .then(data => console.log(data))
+});
+
+// Post request for bitcoin price
+app.get('/btc', (req, res) => {
+    const api_key = '566cd509fae9b7d8663706cef40a59d3f26dbc8e';
+    const url_api = `https://api.nomics.com/v1/currencies/ticker?key=${api_key}&ids=BTC,ETH,XRP&interval=1d`;
+    fetch(url_api)
+        .then(res => res.json())
+        .then(data => res.send(data))
+});
+
+
 
 
 
